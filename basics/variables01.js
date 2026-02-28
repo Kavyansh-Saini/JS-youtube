8e644dd0c11c61b27995ed12bd8d2cf75b8ad639
Kavyansh Saini <146422593+Kavyansh-Saini@users.noreply.github.com>
2026-02-28 02:35:39 +0000

    initialisation

---
+++ basics/variables01.js
@@ -0,0 +1,15 @@
+    const accId=12345
+let accEmail="jack@gmail.com"
+var accPass="123xyz"
+accCity="Rampura"
+
+//accId=2
+accEmail="Jack2@gmail.com"
+accPass=213445
+accCity="Jind"
+
+
+console.log(accId)
+
+console.log([accId,accEmail, accPass, accCity])
+console.table([accId,accEmail, accPass, accCity])
