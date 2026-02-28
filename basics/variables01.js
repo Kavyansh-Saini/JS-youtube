f6d156b93915abbf4d37f1ee8be5e0131e6b96c5
Kavyansh Saini <146422593+Kavyansh-Saini@users.noreply.github.com>
2026-02-28 02:58:40 +0000

    .

--- basics/variables01.js
+++ basics/variables01.js
@@ -2,6 +2,7 @@
 let accEmail="jack@gmail.com"
 var accPass="123xyz"
 accCity="Rampura"
+let accstate
 
 //accId=2
 accEmail="Jack2@gmail.com"
@@ -9,7 +10,14 @@ accPass=213445
 accCity="Jind"
 
 
+/* 
+always use let or const
+
+prefer not use var due to issue in block scope and functional scope
+
+*/
+
 console.log(accId)
 
 console.log([accId,accEmail, accPass, accCity])
-console.table([accId,accEmail, accPass, accCity])
+console.table([accId,accEmail, accPass, accCity, accstate])
