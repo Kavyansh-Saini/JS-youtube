8823ca6938f66332c355fbeed0396aa3239d23f1
Kavyansh Saini <146422593+Kavyansh-Saini@users.noreply.github.com>
2026-03-03 18:35:59 +0530

    appended 05_strings.js

---
+++ basics/05_strings.js
@@ -0,0 +1,34 @@
+// let name= "Kavyansh"
+
+// let age= 20
+
+// console.log("hello " + name + " I am " + age); 
+// // primitive, non-desirable
+
+// console.log(`Hello, my name is ${name} and my age is ${age}`);
+// //string interpolation 
+
+// const gameName=new String("Kavyansh-Saini")
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);    // prototype
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(6));
+// console.log(gameName.indexOf('s'));
+
+// const newString= gameName.substring(0,4)    // 4 index not incl.
+// console.log(newString);
+
+// const anotherString= gameName.slice(-8,-1)   // can use neg values
+// console.log(anotherString);
+
+// const nextName= "       Kavyansh              "
+// console.log(nextName);
+// console.log(nextName.trim());
+
+const Name2= "Kavyansh-Saini"
+console.log(Name2.replace("-", "<>"));
+console.log(Name2.includes("Saini"));
+console.log(Name2.split("-"));
